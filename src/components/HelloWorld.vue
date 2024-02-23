8c55e5b549a98e92417ee20ac1d716a25d583800
huahua1215 <s06u.vm0clgj9@gmail.com>
2024-02-23 10:07:49 +0800

    修改CSS樣式為Tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -116,7 +116,7 @@ const total = () => {
       <!-- 瀏覽商品 -->
       <div class="content box-border w-full shrink-0">
         <div class="product" v-for="product in products">
-          <div class="info-box relative p-2.5 bg-white rounded-md m-5">
+          <div class="shadow-infoBox relative p-2.5 bg-white rounded-md m-5">
             <div class="flex items-center">
               <img class="max-h-24" :src="product.thumb" />
               <div class="text-start">
@@ -152,13 +152,28 @@ const total = () => {
             </div>
             <!-- 購物成功的 icon -->
             <div
-              class="icon-container"
-              :class="{ showing: product.showingIcon }"
+              class="icon-container absolute inset-0 z-10 text-lightRed text-20px bg-lightWhite opacity-0 pointer-events-none pt-30px pr-20px pl-20px"
+              :class="{
+                'showing opacity-100 pointer-events-auto': product.showingIcon,
+              }"
             >
               <p>
-                <svg class="icon" viewBox="0 0 100 100" width="80" height="80">
-                  <circle class="circle" cx="50" cy="50" r="48"></circle>
-                  <polyline class="check" points="28,53 42,66 74,34"></polyline>
+                <svg
+                  class="ml-110px"
+                  viewBox="0 0 100 100"
+                  width="80"
+                  height="80"
+                >
+                  <circle
+                    class="circle fill-none stroke-lightRed stroke-2"
+                    cx="50"
+                    cy="50"
+                    r="48"
+                  ></circle>
+                  <polyline
+                    class="check fill-none stroke-lightRed stroke-2"
+                    points="28,53 42,66 74,34"
+                  ></polyline>
                 </svg>
                 成功加入購物車
               </p>
@@ -168,14 +183,16 @@ const total = () => {
       </div>
       <!-- 購物車 -->
       <div class="content box-border w-full shrink-0">
-        <table>
-          <thead>
+        <table
+          class="m-20px border-spacing-0 border-collapse bg-white shadow-table width-90%"
+        >
+          <thead class="text-lightRed text-16px text-left">
             <tr>
               <th colspan="5">Order</th>
             </tr>
           </thead>
-          <tbody>
-            <tr class="table-title">
+          <tbody class="text-14px">
+            <tr class="txet-c2c2c2 text-12px">
               <td>品項</td>
               <td>數量</td>
               <td>單價</td>
@@ -188,15 +205,23 @@ const total = () => {
               <td>{{ product.price }}</td>
               <td>{{ product.sum }}</td>
               <td>
-                <button class="button-del" @click="remove(product)">X</button>
+                <button
+                  class="outline-0 border-0 pb-0.4em text-b6b6b6 text-18px py-5px px-15px cursor-pointer"
+                  @click="remove(product)"
+                >
+                  X
+                </button>
               </td>
             </tr>
           </tbody>
-          <tfoot>
+          <tfoot class="text-959595 text-12px bg-f9f9f9">
             <tr>
               <td></td>
               <td colspan="3">
-                Total <span class="total-price">$ {{ total() }}</span>
+                Total
+                <span class="ml-base text-black text-20px"
+                  >$ {{ total() }}</span
+                >
               </td>
               <td></td>
             </tr>
@@ -208,12 +233,9 @@ const total = () => {
 </template>
 
 <style scoped>
+/*#region Tab切換頁面之動畫 */
 main {
   .content {
-    /* 讓元素的寬度包含padding+border */
-    /* box-sizing: border-box; */
-    /* 表示當空間不足時，.content 元素不會收縮。 */
-    /* flex-shrink: 0;  */
     transition: transform 300ms;
   }
   &.shift {
@@ -222,52 +244,21 @@ main {
     }
   }
 }
-.info-box {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+/* #endregion */
 
-/* 購物成功的 icon 動畫 */
+/*#region 購物成功的 icon 動畫 */
 .icon-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 10;
-  padding: 30px 20px 0;
-  color: #ef8a8b;
-  font-size: 20px;
-  background: rgba(255, 255, 255, 0.8);
-  opacity: 0;
-  pointer-events: none;
   transition: opacity 0.3s;
-
-  .icon {
-    margin-left: 110px;
-  }
-
-  .circle,
-  .check {
-    fill: none;
-    stroke: #ef8a8b;
-    stroke-width: 3;
-  }
-
   .check {
     stroke-dasharray: 80;
     stroke-dashoffset: 80;
   }
-
   &.showing {
-    opacity: 1;
-    pointer-events: auto;
-
     .check {
       animation: check 0.3s cubic-bezier(0.5, 0, 0.6, 1) forwards 0.2s;
     }
   }
 }
-
 @keyframes check {
   from {
     stroke-dashoffset: 80;
@@ -276,77 +267,22 @@ main {
     stroke-dashoffset: 0;
   }
 }
-/* 購物車表格樣式 */
-table {
-  margin: 20px;
-  border-spacing: 0;
-  border-collapse: collapse;
-  background-color: #fff;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  width: 90%;
+/* #endregion 購物成功的 icon 動畫*/
 
+/*#region 購物車表格樣式 */
+table {
   tr {
     border-bottom: 1px solid #f3f3f3;
   }
-
   th,
   td {
     min-width: 5em;
-    /* width: 20%; */
     padding: 15px 9px;
     text-align: right;
-
     &:first-child {
-      /* width: 40%; */
       text-align: left;
-    }
-
-    /* &:last-child {
-      padding-right: 10px;
-    } */
-  }
-
-  thead {
-    color: #ef8a8b;
-    font-size: 16px;
-    text-align: left;
-
-    .time {
-      color: #c2c2c2;
-      font-size: 14px;
-      text-align: right;
-    }
-  }
-
-  tbody {
-    font-size: 14px;
-
-    .table-title {
-      color: #c2c2c2;
-      font-size: 12px;
-    }
-
-    .button-del {
-      outline: 0;
-      border: 0;
-      padding: 0 0 0.4em;
-      color: #b6b6b6;
-      font-size: 18px;
-      padding: 5px 15px;
-      cursor: pointer;
-    }
-  }
-
-  tfoot {
-    color: #959595;
-    font-size: 12px;
-    background: #f9f9f9;
-
-    .total-price {
-      margin-left: 1em;
-      color: #000;
-      font-size: 20px;
     }
   }
 }
+/* #endregion 購物車表格樣式*/
 </style>
